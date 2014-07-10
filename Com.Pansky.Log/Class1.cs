eab024a94b4e04943a5624378c0bbf29a1955c66
Alex <bittersweeter@hotmail.com>
2014-07-10 21:24:28 +0800

    Framework Design

---
+++ Com.Pansky.Log/Class1.cs
@@ -0,0 +1,22 @@
+﻿/* ======================================================================== 
+* Author：Alex
+* Time：7/10/2014 5:53:22 PM 
+* Version：V1.0.0
+* Company: Pansky 
+* Description:
+* ====================================================================== */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Com.Pansky.Log
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class Class1
+    {
+    }
+}
---
+++ Com.Pansky.SCFDC.SQLServerDAL/Properties/AssemblyInfo.cs
@@ -0,0 +1,43 @@
+﻿/* ======================================================================== 
+* Author：Alex
+* Time：7/10/2014 5:28:56 PM 
+* Version：V1.0.0
+* Company: Pansky 
+* ====================================================================== */
+
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Com.Pansky.SCFDC.SQLServerDAL")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Com.Pansky.SCFDC.SQLServerDAL")]
+[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("faa72845-e0c8-49db-870a-16a1890c8241")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
